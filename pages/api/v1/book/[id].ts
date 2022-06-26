a780adacbd4c8e1148e0b46c7523f900f65c4c8f
Myo Maung <myomaung5255225@gmail.com>
2022-06-26 11:55:52 +0630

    updated_book

---
+++ pages/api/v1/book/[id].ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getBook } from '../../../../lib/controllers/book'
+
+export default async function handler (
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+     await getBook(req,res)
+  } catch (error) {
+    res.status(200).json({
+      statusCode: 500,
+      message: error
+    })
+  }
+}
