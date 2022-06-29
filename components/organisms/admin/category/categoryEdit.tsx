ed5c9ba509eeff7f8119f9e3fe21dd637fbdef15
Hayman <hayman.yoonhanthar@gmail.com>
2022-06-29 14:07:54 +0630

    updated change

---
+++ components/organisms/admin/category/categoryEdit.tsx
@@ -0,0 +1,20 @@
+import React, { Dispatch } from "react";
+import { UiInput } from "../../../atoms/UiInput";
+import { UiSubmit } from "../../../atoms/UiSubmit";
+interface props{
+    formData:any
+    setFormData:Dispatch<React.SetStateAction<any>>
+}
+export const CategoryEditForm:React.FC<props> =({formData,setFormData})=>{
+    return(
+        <div>
+            <UiInput
+                formData={formData}
+                setFormData={setFormData}
+                inputProps={{name:"name",id:'name'}}
+                
+            />
+            <UiSubmit title="Edit"/>
+        </div>
+    )
+}
