2bb7e2aa5ba4092ef2757bc88af7a107676cd525
MyoMaung <myomaung5255225@gmail.com>
2022-06-29 12:08:52 +0630

    updated

--- components/organisms/herocontent/index.tsx
+++ components/organisms/herocontent/index.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import React from 'react';
 import { commonProps, serviceProps, silderProps } from '../../../pages';
+import { RightArrow } from '../../atoms/icons/rightArrow';
 import { UiCarousel } from '../../atoms/UiCarousel';
 import { UiService } from '../../atoms/UiServices';
 
@@ -12,6 +14,13 @@ export const HeroContent:React.FC<commonProps> =({services,sliders})=>{
                     sliders && 
                     <UiCarousel sliders={sliders} />
                 }
+                <div className='flex justify-center w-full  px-2 py-2 my-3 content-center items-center '>
+                    <Link href="/category">
+                       <a className='bg-secondary animate-pulse px-2 py-2 shadow-lg rounded-full  text-inverse'>
+                         <RightArrow className='w-8 h-8' />
+                       </a>
+                    </Link>
+                </div>
                 <UiService services={services} />
 
         </div>
