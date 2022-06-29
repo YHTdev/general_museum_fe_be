0ba815b137dd36c60cb1dfe5ff279cdf4b54708f
Hayman <hayman.yoonhanthar@gmail.com>
2022-06-29 12:11:17 +0630

    admin book category updated

--- pages/admin/book/index.tsx
+++ pages/admin/book/index.tsx
@@ -1,11 +1,52 @@
 import { NextPage } from "next";
-import { BookForm } from "../../../components/organisms/admin/book";
+import { useState,useCallback,useEffect } from "react";
+import { BookForm, bookProps } from "../../../components/organisms/admin/book";
 import { AppWrapper } from "../../../components/templates/AdminLayout";
+import { API } from "../../../lib/services";
 
 const BookAdmin:NextPage =()=>{
+    const [books,setBooks] = useState([]);
+    const id=1;
+    const getBooks = useCallback(
+     () => {
+         API.get('v1/book')
+         .then(res=>{
+             
+             if(res.data.statusCode=== 200){
+                 setBooks(res.data.data)
+             }
+             
+         })
+         .catch(err =>{
+             console.log(JSON.stringify(err));
+         })
+     },
+     [id],
+    )
+   
+    useEffect(() => {
+     getBooks()
+     console.log(books,"Books")
+    }, [getBooks])
+    const editHandler=(book:bookProps)=>{
+        console.log(book);
+    }
+    const deleteHandler=(id:string)=>{
+        
+        API.delete('/v1/admin/book',{data:{
+            id:id
+        }}).then(res => {
+            
+            if(res.data.statusCode === 200){
+                getBooks()
+            }
+        }).catch(err => {
+            console.log(JSON.stringify(err))
+        })
+    }
     return(
         <AppWrapper>
-            <BookForm />
+           { books && <BookForm books={books} editHandler={editHandler} deleteHandler={deleteHandler}/>}
         </AppWrapper>
     )
 }
