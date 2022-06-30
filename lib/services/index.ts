f0e3b31b7f131a472f8d07319d3cecc31c3953f0
ArkarPhyoMichael <arkarphyo.yoonhanthar@gmail.com>
2022-06-30 16:42:35 +0630

    make api works on ip

--- lib/services/index.ts
+++ lib/services/index.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const API = axios.create({
-    baseURL:"http://localhost:3000/api/",
-    withCredentials:true
-})
+  baseURL: `${process.env.NEXT_PUBLIC_NEXTAPI_URL}`,
+  withCredentials: true,
+});
--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  responseLimit: '8mb',
+  responseLimit: "8mb",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://192.168.30.126:3000/:path*",
+      },
+    ];
+  },
   api: {
     bodyParser: {
-        sizeLimit: '8mb' // Set desired value here
-    }
-}
-}
+      sizeLimit: "8mb", // Set desired value here
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
