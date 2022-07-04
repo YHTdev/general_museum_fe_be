a2847b55472d7f8c8a5b11baeb74e843f0eafef6
ArkarPhyoMichael <arkarphyo.yoonhanthar@gmail.com>
2022-07-04 16:21:22 +0630

    change ip server

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  responseLimit: '8mb',
+  responseLimit: "8mb",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_NEXTAPI_URL}/:path*`,
+      },
+    ];
+  },
   api: {
     bodyParser: {
-        sizeLimit: '8mb' // Set desired value here
-    }
-}
-}
+      sizeLimit: "8mb", // Set desired value here
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
